ab4379f3a761b628e852bf42822ec07becace44f
Federico Ayala <fedeayala08@hotmail.com>
2023-06-20 18:55:52 +0200

    Add proyect class 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { MainWrapperComponent } from './main-wrapper/main-wrapper.component';
+import { MyComponent } from './my-component/my-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    MainWrapperComponent,
+    MyComponent
   ],
   imports: [
     BrowserModule,
